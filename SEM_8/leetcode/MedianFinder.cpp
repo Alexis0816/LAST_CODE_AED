dd5485e5502a2541dbb03d14d003b799ad6ca965
Alexis Raza <gilver.raza@utec.edu.pe>
2024-05-21 11:04:43 -0500

    update

--- SEM_8/leetcode/MedianFinder.cpp
+++ SEM_8/leetcode/MedianFinder.cpp
@@ -1,42 +1,363 @@
+// #include <iostream>
+// #include <queue>
+// #include <vector>
+
+// class MedianFinder {
+// private:
+//     std::priority_queue<int> maxHeap; // max-heap para la mitad menor
+//     std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap; // min-heap para la mitad mayor
+
+// public:
+//     /** initialize your data structure here. */
+//     MedianFinder() {}
+
+//     void addNum(int num) {
+//         // Insertar en el max-heap primero
+//         maxHeap.push(num);
+        
+//         // Asegurarse de que el mayor del max-heap sea menor que el menor del min-heap
+//         if (!minHeap.empty() && maxHeap.top() > minHeap.top()) {
+//             int maxTop = maxHeap.top();
+//             maxHeap.pop();
+//             minHeap.push(maxTop);
+//         }
+
+//         // Balancear los heaps si es necesario
+//         if (maxHeap.size() > minHeap.size() + 1) {
+//             minHeap.push(maxHeap.top());
+//             maxHeap.pop();
+//         } else if (minHeap.size() > maxHeap.size()) {
+//             maxHeap.push(minHeap.top());
+//             minHeap.pop();
+//         }
+//     }
+
+//     double findMedian() {
+//         if (maxHeap.size() == minHeap.size()) {
+//             return (maxHeap.top() + minHeap.top()) / 2.0;
+//         } else {
+//             return maxHeap.top();
+//         }
+//     }
+// };
+
+// int main() {
+//     MedianFinder medianFinder;
+//     medianFinder.addNum(2);
+//     medianFinder.addNum(3);
+//     std::cout << "Mediana: " << medianFinder.findMedian() << std::endl; // 1.5
+//     medianFinder.addNum(4);
+//     std::cout << "Mediana: " << medianFinder.findMedian() << std::endl; // 2
+//     return 0;
+// }
+
+
+// /**
+//  * Your MedianFinder object will be instantiated and called as such:
+//  * MedianFinder* obj = new MedianFinder();
+//  * obj->addNum(num);
+//  * double param_2 = obj->findMedian();
+//  */
+
+
+
+
 #include <iostream>
-#include <queue>
-#include <vector>
+
+template <typename T>
+class MaxHeap {
+private:
+    struct Node {
+        T data;
+        Node* left;
+        Node* right;
+        Node(T data) : data(data), left(nullptr), right(nullptr) {}
+    };
+
+    Node* root;
+
+    Node* insert(Node* root, T data) {
+        if (root == nullptr) {
+            return new Node(data);
+        }
+
+        if (data > root->data) {
+            std::swap(root->data, data);
+        }
+
+        if (root->left == nullptr) {
+            root->left = insert(root->left, data);
+        } else if (root->right == nullptr) {
+            root->right = insert(root->right, data);
+        } else if (root->left->data < root->right->data) {
+            root->left = insert(root->left, data);
+        } else {
+            root->right = insert(root->right, data);
+        }
+
+        return root;
+    }
+
+    Node* deleteMax(Node* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        if (root->left == nullptr && root->right == nullptr) {
+            delete root;
+            return nullptr;
+        }
+
+        if (root->left == nullptr || root->right == nullptr) {
+            Node* temp = (root->left != nullptr) ? root->left : root->right;
+            delete root;
+            return temp;
+        }
+
+        if (root->left->data > root->right->data) {
+            std::swap(root->data, root->left->data);
+            root->left = deleteMax(root->left);
+        } else {
+            std::swap(root->data, root->right->data);
+            root->right = deleteMax(root->right);
+        }
+
+        return root;
+    }
+
+    void heapifyUp(Node* node) {
+        if (node == nullptr || node == root) {
+            return;
+        }
+
+        if (node->data > node->right->data) {
+            std::swap(node->data, node->right->data);
+            heapifyUp(node->right);
+        } else if (node->data > node->left->data) {
+            std::swap(node->data, node->left->data);
+            heapifyUp(node->left);
+        }
+    }
+
+    void heapifyDown(Node* node) {
+        if (node == nullptr) {
+            return;
+        }
+
+        if (node->left != nullptr && node->left->data > node->data) {
+            std::swap(node->data, node->left->data);
+            heapifyDown(node->left);
+        } else if (node->right != nullptr && node->right->data > node->data) {
+            std::swap(node->data, node->right->data);
+            heapifyDown(node->right);
+        }
+    }
+
+public:
+    MaxHeap() : root(nullptr) {}
+
+    void insert(T data) {
+        root = insert(root, data);
+        heapifyUp(root);
+    }
+
+    T extractMax() {
+        if (root == nullptr) {
+            throw "Heap vacío";
+        }
+
+        T maxData = root->data;
+        root = deleteMax(root);
+        heapifyDown(root);
+
+        return maxData;
+    }
+
+    T max() {
+        if (root == nullptr) {
+            throw "Heap vacío";
+        }
+
+        return root->data;
+    }
+
+    void display() {
+        display(root);
+        std::cout << std::endl;
+    }
+
+private:
+    void display(Node* root) {
+        if (root == nullptr) {
+            return;
+        }
+
+        display(root->left);
+        std::cout << root->data << " ";
+        display(root->right);
+    }
+};
+
+template <typename T>
+class MinHeap {
+private:
+    struct Node {
+        T data;
+        Node* left;
+        Node* right;
+        Node(T data) : data(data), left(nullptr), right(nullptr) {}
+    };
+
+    Node* root;
+
+    Node* insert(Node* root, T data) {
+        if (root == nullptr) {
+            return new Node(data);
+        }
+
+        if (data < root->data) {
+            root->left = insert(root->left, data);
+        } else {
+            root->right = insert(root->right, data);
+        }
+
+        return root;
+    }
+
+    Node* findMin(Node* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        while (root->left != nullptr) {
+            root = root->left;
+        }
+
+        return root;
+    }
+
+    Node* deleteMin(Node* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        if (root->left == nullptr) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        root->left = deleteMin(root->left);
+
+        if (root->left != nullptr && root->right != nullptr) {
+            if (root->left->data < root->right->data) {
+                std::swap(root->data, root->left->data);
+            } else {
+                std::swap(root->data, root->right->data);
+            }
+        }
+
+        return root;
+    }
+
+    void heapifyUp(Node* node) {
+        if (node == nullptr || node == root) {
+            return;
+        }
+
+        if (node->data < node->right->data) {
+            std::swap(node->data, node->right->data);
+            heapifyUp(node->right);
+        } else if (node->data < node->left->data) {
+            std::swap(node->data, node->left->data);
+            heapifyUp(node->left);
+        }
+    }
+
+    void heapifyDown(Node* node) {
+        if (node == nullptr) {
+            return;
+        }
+
+        if (node->left != nullptr && node->left->data < node->data) {
+            std::swap(node->data, node->left->data);
+            heapifyDown(node->left);
+        } else if (node->right != nullptr && node->right->data < node->data) {
+            std::swap(node->data, node->right->data);
+            heapifyDown(node->right);
+        }
+    }
+
+public:
+    MinHeap() : root(nullptr) {}
+
+    void insert(T data) {
+        root = insert(root, data);
+        heapifyUp(root);
+    }
+
+    T extractMin() {
+        if (root == nullptr) {
+            throw "Heap vacío";
+        }
+
+        Node* minNode = findMin(root);
+        T minData = minNode->data;
+        root = deleteMin(root);
+        heapifyDown(root);
+
+        return minData;
+    }
+
+    T min() {
+        return root->data;
+    }
+
+    void display() {
+        display(root);
+        std::cout << std::endl;
+    }
+
+private:
+    void display(Node* root) {
+        if (root == nullptr) {
+            return;
+        }
+
+        display(root->left);
+        std::cout << root->data << " ";
+        display(root->right);
+    }
+};
 
 class MedianFinder {
 private:
-    std::priority_queue<int> maxHeap; // max-heap para la mitad menor
-    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap; // min-heap para la mitad mayor
+    MaxHeap<int> maxHeap; // max-heap for the lower half
+    MinHeap<int> minHeap; // min-heap for the upper half
 
 public:
-    /** initialize your data structure here. */
     MedianFinder() {}
 
     void addNum(int num) {
-        // Insertar en el max-heap primero
-        maxHeap.push(num);
-        
-        // Asegurarse de que el mayor del max-heap sea menor que el menor del min-heap
-        if (!minHeap.empty() && maxHeap.top() > minHeap.top()) {
-            int maxTop = maxHeap.top();
-            maxHeap.pop();
-            minHeap.push(maxTop);
+        if (maxHeap.root == nullptr || num <= maxHeap.max()) {
+            maxHeap.insert(num);
+        } else {
+            minHeap.insert(num);
         }
 
-        // Balancear los heaps si es necesario
+        // Balance heaps
         if (maxHeap.size() > minHeap.size() + 1) {
-            minHeap.push(maxHeap.top());
-            maxHeap.pop();
+            minHeap.insert(maxHeap.extractMax());
         } else if (minHeap.size() > maxHeap.size()) {
-            maxHeap.push(minHeap.top());
-            minHeap.pop();
+            maxHeap.insert(minHeap.extractMin());
         }
     }
 
     double findMedian() {
         if (maxHeap.size() == minHeap.size()) {
-            return (maxHeap.top() + minHeap.top()) / 2.0;
+            return (maxHeap.max() + minHeap.min()) / 2.0;
         } else {
-            return maxHeap.top();
+            return maxHeap.max();
         }
     }
 };
@@ -45,16 +366,8 @@ int main() {
     MedianFinder medianFinder;
     medianFinder.addNum(2);
     medianFinder.addNum(3);
-    std::cout << "Mediana: " << medianFinder.findMedian() << std::endl; // 1.5
+    std::cout << "Mediana: " << medianFinder.findMedian() << std::endl; // 2.5
     medianFinder.addNum(4);
-    std::cout << "Mediana: " << medianFinder.findMedian() << std::endl; // 2
+    std::cout << "Mediana: " << medianFinder.findMedian() << std::endl; // 3
     return 0;
 }
-
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
--- SEM_8/leetcode/kWeakestRows.cpp
+++ SEM_8/leetcode/kWeakestRows.cpp
@@ -1,59 +1,59 @@
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <iostream>
+// }#include <vector>
+// #include <queue>
+// #include <algorithm>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-class Solution
-{
-    struct Node
-    {
-        int _sum{};
-        int _index{};
-        Node(int sum, int index) : _sum(sum), _index(index) {}
-        ~Node() {}
-    };
+// class Solution
+// {
+//     struct Node
+//     {
+//         int _sum{};
+//         int _index{};
+//         Node(int sum, int index) : _sum(sum), _index(index) {}
+//         ~Node() {}
+//     };
 
-    int sum(vector<int> &a)
-    {
-        int res = 0;
-        for (auto e : a)
-        {
-            res += e;
-        }
-        return res;
-    }
+//     int sum(vector<int> &a)
+//     {
+//         int res = 0;
+//         for (auto e : a)
+//         {
+//             res += e;
+//         }
+//         return res;
+//     }
 
-public:
-    vector<int> kWeakestRows(vector<vector<int>> &mat, int k)
-    {
-        auto lesserNode = [](Node &a, Node &b)
-        {
-            if (a._sum != b._sum)
-            {
-                return a._sum > b._sum;
-            }
-            return a._index > b._index;
-        };
+// public:
+//     vector<int> kWeakestRows(vector<vector<int>> &mat, int k)
+//     {
+//         auto lesserNode = [](Node &a, Node &b)
+//         {
+//             if (a._sum != b._sum)
+//             {
+//                 return a._sum > b._sum;
+//             }
+//             return a._index > b._index;
+//         };
 
-        std::priority_queue<Node, std::vector<Node>, decltype(lesserNode)> queue(lesserNode);
-        for (int i = 0; i < mat.size(); ++i)
-        {
-            int val = sum(mat[i]);
-            Node n = Node(val, i);
-            queue.push(n);
-        }
-        vector<int> kWeakest;
-        for (int i = 0; i < k; ++i)
-        {
-            int index = queue.top()._index;
-            kWeakest.push_back(index);
-            queue.pop();
-        }
-        return kWeakest;
-    }
-};
+//         std::priority_queue<Node, std::vector<Node>, decltype(lesserNode)> queue(lesserNode);
+//         for (int i = 0; i < mat.size(); ++i)
+//         {
+//             int val = sum(mat[i]);
+//             Node n = Node(val, i);
+//             queue.push(n);
+//         }
+//         vector<int> kWeakest;
+//         for (int i = 0; i < k; ++i)
+//         {
+//             int index = queue.top()._index;
+//             kWeakest.push_back(index);
+//             queue.pop();
+//         }
+//         return kWeakest;
+//     }
+// };
 
 // int main()
 // {
@@ -72,3 +72,132 @@ public:
 //     std::cout << std::endl;
 //     return 0;
 // }
+
+
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class MinHeap {
+private:
+    vector<T> heap;
+
+    void heapifyUp(int index) {
+        if (index == 0) return;
+        int parentIndex = (index - 1) / 2;
+        if (heap[index] < heap[parentIndex]) {
+            swap(heap[index], heap[parentIndex]);
+            heapifyUp(parentIndex);
+        }
+    }
+
+    void heapifyDown(int index) {
+        int smallest = index;
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+
+        if (left < heap.size() && heap[left] < heap[smallest]) {
+            smallest = left;
+        }
+
+        if (right < heap.size() && heap[right] < heap[smallest]) {
+            smallest = right;
+        }
+
+        if (smallest != index) {
+            swap(heap[index], heap[smallest]);
+            heapifyDown(smallest);
+        }
+    }
+
+public:
+    MinHeap() {}
+
+    void insert(T data) {
+        heap.push_back(data);
+        heapifyUp(heap.size() - 1);
+    }
+
+    T extractMin() {
+        if (heap.empty()) {
+            throw "Heap vacío";
+        }
+
+        T minData = heap[0];
+        heap[0] = heap.back();
+        heap.pop_back();
+        heapifyDown(0);
+
+        return minData;
+    }
+
+    bool isEmpty() const {
+        return heap.empty();
+    }
+};
+
+class Solution
+{
+    struct Node
+    {
+        int _sum{};
+        int _index{};
+        Node(int sum, int index) : _sum(sum), _index(index) {}
+
+        bool operator<(const Node &other) const {
+            if (_sum != other._sum) {
+                return _sum < other._sum;
+            }
+            return _index < other._index;
+        }
+    };
+
+    int sum(vector<int> &a)
+    {
+        int res = 0;
+        for (auto e : a)
+        {
+            res += e;
+        }
+        return res;
+    }
+
+public:
+    vector<int> kWeakestRows(vector<vector<int>> &mat, int k)
+    {
+        MinHeap<Node> heap;
+        for (int i = 0; i < mat.size(); ++i)
+        {
+            int val = sum(mat[i]);
+            Node n(val, i);
+            heap.insert(n);
+        }
+        vector<int> kWeakest;
+        for (int i = 0; i < k; ++i)
+        {
+            kWeakest.push_back(heap.extractMin()._index);
+        }
+        return kWeakest;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> mat = {
+        {1, 1, 0, 0, 0},
+        {1, 1, 1, 1, 0},
+        {1, 0, 0, 0, 0},
+        {1, 1, 0, 0, 0},
+        {1, 1, 1, 1, 1}};
+
+    auto res = Solution().kWeakestRows(mat, 3);
+    for (auto e : res)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
