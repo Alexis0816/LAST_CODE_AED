f3315118711e1cc442b7b6623250556c682234f5
Alexis Raza <gilver.raza@utec.edu.pe>
2024-07-23 16:15:53 -0500

    update CLL

--- SEM_3/Circular/CircularDoublyLinkedList.cpp
+++ SEM_3/Circular/CircularDoublyLinkedList.cpp
@@ -1,8 +1,8 @@
-#include "CircularLinkedList.hpp"
+#include "CircularDoublyLinkedList.hpp"
 
 int main()
 {
-  CircularLinkedList list;
+  CircularDoublyLinkedList list;
   list.push_back(1);
   list.push_back(2);
   list.push_back(3);
